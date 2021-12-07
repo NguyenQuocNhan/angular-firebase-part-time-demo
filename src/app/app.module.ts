d8270a51b06d5b688d6aa96ac594669e5a424b18
NguyenQuocNhan <nguyenquocnhan777@gmail.com>
2021-12-07 12:57:51 +0700

    'update'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AngularFirestoreModule } from  '@angular//fire/compat/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyBRrIRwd2ny6VwXfREfVmytTUsw4srL3WY",
+      authDomain: "part-time-demon.firebaseapp.com",
+      projectId: "part-time-demon",
+      storageBucket: "part-time-demon.appspot.com",
+      messagingSenderId: "324796452134",
+      appId: "1:324796452134:web:7360f44632cbe0ff8e05e7"
+    }),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
